e6a07ff4ac45c660c5b0c1fb54130a4340b44a82
pablodecm <pablodecm@gmail.com>
2015-05-20 17:19:04 +0200

    Create basic candidate class

---
+++ interface/Candidate.h
@@ -0,0 +1,30 @@
+
+#include "Math/Vector4D.h"
+#include "Math/Vector4Dfwd.h"
+
+namespace mut {
+  
+  typedef ROOT::Math::PtEtaPhiEVector PtEtaPhiEVector;
+
+  class Candidate : public PtEtaPhiEVector {
+
+    public:
+
+      // inherit constructors
+      using PtEtaPhiEVector::PtEtaPhiEVector;
+
+      virtual ~Candidate() {}
+      inline int charge() const { return qx3_/3; }
+      inline void setCharge(int q) { qx3_ = q*3; }
+      inline int threeCharge() const { return qx3_; }
+      inline void setThreeCharge(int qx3) { qx3_ = qx3; }
+      inline int pdgId() const { return pdgId_; }
+      inline void setPdgId(int pdgId) { pdgId_ = pdgId; }
+
+    protected:
+      int qx3_;    // threeCharge
+      int pdgId_;  // PDG id
+
+  };
+
+}
