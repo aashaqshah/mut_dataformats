3ac29c702be12d7ad6d9dfa28f41bb4a87812a66
Pablo de Castro <pablodecm@gmail.com>
2015-06-04 12:33:51 -0400

    CMake build system and ROOT6 dict genaration

---
+++ interface/LinkDef.h
@@ -0,0 +1,14 @@
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class mut::EventInfo+;
+#pragma link C++ class mut::Candidate+;
+#pragma link C++ class mut::Lepton+;
+#pragma link C++ class std::vector<mut::Lepton>+;
+#pragma link C++ class mut::Jet+;
+#pragma link C++ class std::vector<mut::Jet>+;
+#pragma link C++ class mut::MET+;
+
+#endif
