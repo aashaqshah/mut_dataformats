2fb40ac473ac63cf8ffca6a02a33fa91ac71fbc8
Pablo de Castro <pablodecm@gmail.com>
2015-06-04 14:33:24 -0400

    Configuration for SCRAM build (CMSSW)

---
+++ src/classes.h
@@ -0,0 +1,20 @@
+#include <vector>
+
+#include "mut_framework/mut_dataformats/interface/EventInfo.h"
+#include "mut_framework/mut_dataformats/interface/Candidate.h"
+#include "mut_framework/mut_dataformats/interface/Lepton.h"
+#include "mut_framework/mut_dataformats/interface/Jet.h"
+#include "mut_framework/mut_dataformats/interface/MET.h"
+
+namespace { struct dictionary {
+  mut::EventInfo dummy0;
+  mut::Candidate dummy1;
+  std::vector<mut::Candidate> dummy2;
+  mut::Lepton dummy3;
+  std::vector<mut::Lepton> dummy4;
+  mut::Jet dummy5;
+  std::vector<mut::Jet> dummy6;
+  mut::MET dummy7;
+};
+}
+
